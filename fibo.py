b07e74742459f23da9178839a6dd873eeac0520a
246994 <246994@vutbr.cz>
2025-04-28 09:26:31 +0200

    (carbonara)

--- fibo.py
+++ fibo.py
@@ -1,6 +1,17 @@
+def recursife_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
 
+    else:
+        return recursife_nth_fibo(n-1) + recursife_nth_fibo(n-2)
+# print(recursife_nth_fibo(5))
 def main():
-    pass
+    n = input("Zadej idx v fibomacchci posloupnosti: ")
+    n = int(n)
+    vejsledek = recursife_nth_fibo(n)
+    return print(vejsledek)
 
 
 if __name__ == "__main__":
